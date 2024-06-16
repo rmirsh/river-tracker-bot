9a9ae79c4a88f49c6af39bbed41fc36ba52b62dd
Kamil Ayupov <ayupovkamil@gmail.com>
2024-06-16 18:38:53 +0300

    Add notifications by user ID

--- bot/utils/notifications.py
+++ bot/utils/notifications.py
@@ -2,16 +2,26 @@ import asyncio
 
 from aiogram import Bot
 
-from emercit_parse.async_emercit_data import a_get_river_data, towns
+from bot.db.requests import get_subs_chat_id
+from emercit_parse.async_emercit_data import async_get_river_data, towns
 
 
-async def send_warning(bot: Bot):
+async def send_warning(bot: Bot) -> None:
     while True:
         for town in towns:
-            river_data = await a_get_river_data(town)
+            river_data = await async_get_river_data(town)
             if river_data.current_river_level < river_data.prevention_level:
-                await bot.send_message(446913605, f"{town}")
-                await asyncio.sleep(2)
+                subs_id = await get_subs_chat_id()
+                subs_id_integers = convert_nested_list_to_integers(subs_id)
+                for sub_id in subs_id_integers:
+                    await bot.send_message(sub_id, f"{town}")
+                    await asyncio.sleep(2)
+
+
+def convert_nested_list_to_integers(nested_list: list[list[str]]) -> list[int]:
+    list_of_integers = [int(string) for row in nested_list for string in row]
+
+    return list_of_integers
 
 
 async def on_startup(bot: Bot):
