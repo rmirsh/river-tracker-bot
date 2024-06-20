ab137365ab5bfca47c91ae281d3b6a92256fa109
Kamil Ayupov <ayupovkamil@gmail.com>
2024-06-20 09:34:04 +0300

    Add env vars for production

--- config.py
+++ config.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     DB_URL: str = Field(env="DB_URL")
     TOKEN: str = Field(env="TOKEN")
     DELAY: int = Field(env="DELAY", default=30 * 60)
+    POSTGRES_HOST_AUTH_METHOD: str = Field(env="POSTGRES_HOST_AUTH_METHOD")
+    POSTGRES_USER: str = Field(env="POSTGRES_USER")
+    POSTGRES_PASSWORD: str | None = Field(env="POSTGRES_PASSWORD")
+    POSTGRES_DB: str = Field(env="POSTGRES_DB")
 
     model_config = SettingsConfigDict(env_file=".env_prod", env_file_encoding="utf-8")
 
