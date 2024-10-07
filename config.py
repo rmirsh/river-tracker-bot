ea60275c4088ce9074443a8ae4acba80f805cf76
Kamil Ayupov <ayupovkamil@gmail.com>
2024-10-07 19:43:20 +0300

    Fixed namings

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from pydantic import Field
 
 
 class Settings(BaseSettings):
-    DB_URL: str = Field(env="DB_URL")
-    TOKEN: str = Field(env="TOKEN")
-    DELAY: int = Field(env="DELAY", default=30 * 60)
-    POSTGRES_HOST_AUTH_METHOD: str = Field(env="POSTGRES_HOST_AUTH_METHOD", default="")
-    POSTGRES_USER: str = Field(env="POSTGRES_USER")
-    POSTGRES_PASSWORD: str | None = Field(env="POSTGRES_PASSWORD")
-    POSTGRES_DB: str = Field(env="POSTGRES_DB", default="")
+    db_url: str = Field(env="DB_URL")
+    token: str = Field(env="TOKEN")
+    delay: int = Field(env="DELAY", default=30 * 60)
+    postgres_host_auth_method: str = Field(env="POSTGRES_HOST_AUTH_METHOD", default="")
+    postgres_user: str = Field(env="POSTGRES_USER")
+    postgres_password: str | None = Field(env="POSTGRES_PASSWORD")
+    postgres_db: str = Field(env="POSTGRES_DB", default="")
 
     model_config = SettingsConfigDict(env_file=".env_prod", env_file_encoding="utf-8")
 
