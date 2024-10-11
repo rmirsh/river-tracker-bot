e8a5a3d37ec1c042ddb2b36d8c89b0cf25222045
Kamil Ayupov <ayupovkamil@gmail.com>
2024-10-11 15:43:29 +0300

    Wrap main func in try except

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
-from bot.create_bot import create_bot, setup_dispather
+from bot.create_bot import setup_bot, setup_dispather
 
 
 async def main():
-    bot = await create_bot()
-    dp = await setup_dispather()
-    await dp.start_polling(bot)
-
+    try:
+        bot = await setup_bot()
+        dp = await setup_dispather()
+        await dp.start_polling(bot)
+    except Exception as e:
+        print(f"An error occured: {e}!")
 
 if __name__ == "__main__":
     import asyncio
