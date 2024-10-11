47d19203ada43d768add04d4dbcbda949c6dca9c
Kamil Ayupov <ayupovkamil@gmail.com>
2024-10-11 15:54:19 +0300

    Added args for bot creation

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from bot.create_bot import setup_bot, setup_dispather
+from config import settings
 
 
 async def main():
     try:
-        bot = await setup_bot()
+        bot = await setup_bot(
+            delete_webhooks=True if not settings.general.is_prod else False,
+        )
         dp = await setup_dispather()
         await dp.start_polling(bot)
     except Exception as e:
